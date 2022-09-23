killcmd/hardware_oplus

hardware: Initial OPlusExtras 7.0 import!
- Now using a better resource based configuration method to allow
  universally any Oplus device to ship this package and configure it
  by using overlays in their device specific tree.

Co-authored-by: Max Weninger <max.weninger@gmail.com>
Co-authored-by: oddlyspaced <oddlyspaced1@gmail.com>
Co-authored-by: TheScarastic <warabhishek@gmail.com>
Co-authored-by: LuK1337 <priv.luk@gmail.com>
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: lostark13 <tushar9915@gmail.com>
Co-authored-by: cristianvaz <cristianvaz81993@gmail.com>
Co-authored-by: jhenrique09 <jhsv09@gmail.com>
Co-authored-by: AliHasan7671 <AHB7671@gmail.com>
Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
Co-authored-by: darkobas <darkobas@gmail.com>
Co-authored-by: Vachounet <vachounet@live.fr>
Co-authored-by: kldoc <fred.berbigier@gmail.com>
Co-authored-bt: Nauval Rizky <enuma.alrizky@gmail.com>
Co-authored-by: Christian Oder <myself5@carbonrom.org>
Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
Co-authored-by: SiddharthBharadwaj <siddharthbharadwaj45@gmail.com>
Co-authored-by: Michele Bono <bono.michele94@gmail.com>
Co-authored-by: LibXZR <xzr467706992@163.com>
Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
Co-authored-by: Joey Huab <joey@evolution-x.org>
Signed-off-by: AnierinB <anierin@evolution-x.org>

OPlusExtras/src/org/evolution/oplus/OPlusExtras/Preferences/ContrastPreference.java
@@ -1,0 +1,94 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+* Copyright (C) 2021-2022 The Evolution X Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.com/licenses/>.
+*
+*/
+package org.evolution.oplus.OPlusExtras.preferences;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceViewHolder;
+import android.util.AttributeSet;
+
+import org.evolution.oplus.OPlusExtras.kcal.KcalSettings;
+import org.evolution.oplus.OPlusExtras.FileUtils;
+import org.evolution.oplus.OPlusExtras.R;
+
+public class ContrastPreference extends CustomSeekBarPreference {
+
+    private static int mDefVal;
+
+    private static final int NODE_LEVEL = R.string.node_contrast_preference;
+
+    public ContrastPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mInterval = context.getResources().getInteger(R.integer.kcal_contrast_preference_interval);
+        mShowSign = false;
+        mUnits = "";
+        mContinuousUpdates = false;
+
+        int[] mAllValues = context.getResources().getIntArray(R.array.kcal_contrast_preference_array);
+        mMinValue = mAllValues[1];
+        mMaxValue = mAllValues[2];
+        mDefaultValueExists = true;
+        mDefVal = mAllValues[0];
+        mDefaultValue = mDefVal;
+        mValue = Integer.parseInt(loadValue(context));
+
+        setPersistent(false);
+    }
+
+    private static String getFile(Context context) {
+        String file = context.getString(NODE_LEVEL);
+        if (FileUtils.fileWritable(file)) {
+            return file;
+        }
+        return null;
+    }
+
+    public static boolean isSupported(Context context) {
+        return FileUtils.fileWritable(getFile(context));
+    }
+
+    public static void restore(Context context) {
+        if (!isSupported(context)) {
+            return;
+        }
+        
+        int[] mAllValues = context.getResources().getIntArray(R.array.kcal_contrast_preference_array);
+        mDefVal = mAllValues[0];
+        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(KcalSettings.KEY_CONTRAST, String.valueOf(mDefVal));
+        FileUtils.writeValue(getFile(context), storedValue);
+    }
+
+    public static String loadValue(Context context) {
+        return FileUtils.getFileValue(getFile(context), String.valueOf(mDefVal));
+    }
+
+    private void saveValue(String newValue) {
+        FileUtils.writeValue(getFile(getContext()), newValue);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        editor.putString(KcalSettings.KEY_CONTRAST, newValue);
+        editor.apply();
+    }
+
+    @Override
+    protected void changeValue(int newValue) {
+        saveValue(String.valueOf(newValue));
+    }
+}

OPlusExtras/src/org/evolution/oplus/OPlusExtras/Preferences/GreenPreference.java
@@ -1,0 +1,94 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+* Copyright (C) 2021-2022 The Evolution X Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.com/licenses/>.
+*
+*/
+package org.evolution.oplus.OPlusExtras.preferences;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceViewHolder;
+import android.util.AttributeSet;
+
+import org.evolution.oplus.OPlusExtras.kcal.KcalSettings;
+import org.evolution.oplus.OPlusExtras.FileUtils;
+import org.evolution.oplus.OPlusExtras.R;
+
+public class GreenPreference extends CustomSeekBarPreference {
+
+    private static int mDefVal;
+
+    private static final int NODE_LEVEL = R.string.node_green_preference;
+
+    public GreenPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mInterval = context.getResources().getInteger(R.integer.kcal_green_preference_interval);
+        mShowSign = false;
+        mUnits = "";
+        mContinuousUpdates = false;
+
+        int[] mAllValues = context.getResources().getIntArray(R.array.kcal_green_preference_array);
+        mMinValue = mAllValues[1];
+        mMaxValue = mAllValues[2];
+        mDefaultValueExists = true;
+        mDefVal = mAllValues[0];
+        mDefaultValue = mDefVal;
+        mValue = Integer.parseInt(loadValue(context));
+
+        setPersistent(false);
+    }
+
+    private static String getFile(Context context) {
+        String file = context.getString(NODE_LEVEL);
+        if (FileUtils.fileWritable(file)) {
+            return file;
+        }
+        return null;
+    }
+
+    public static boolean isSupported(Context context) {
+        return FileUtils.fileWritable(getFile(context));
+    }
+
+    public static void restore(Context context) {
+        if (!isSupported(context)) {
+            return;
+        }
+        
+        int[] mAllValues = context.getResources().getIntArray(R.array.kcal_green_preference_array);
+        mDefVal = mAllValues[0];
+        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(KcalSettings.KEY_GREEN, String.valueOf(mDefVal));
+        FileUtils.writeValue(getFile(context), storedValue);
+    }
+
+    public static String loadValue(Context context) {
+        return FileUtils.getFileValue(getFile(context), String.valueOf(mDefVal));
+    }
+
+    private void saveValue(String newValue) {
+        FileUtils.writeValue(getFile(getContext()), newValue);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        editor.putString(KcalSettings.KEY_GREEN, newValue);
+        editor.apply();
+    }
+
+    @Override
+    protected void changeValue(int newValue) {
+        saveValue(String.valueOf(newValue));
+    }
+}

OPlusExtras/src/org/evolution/oplus/OPlusExtras/Preferences/SaturationPreference.java
@@ -1,0 +1,94 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+* Copyright (C) 2021-2022 The Evolution X Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.com/licenses/>.
+*
+*/
+package org.evolution.oplus.OPlusExtras.preferences;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceViewHolder;
+import android.util.AttributeSet;
+
+import org.evolution.oplus.OPlusExtras.kcal.KcalSettings;
+import org.evolution.oplus.OPlusExtras.FileUtils;
+import org.evolution.oplus.OPlusExtras.R;
+
+public class SaturationPreference extends CustomSeekBarPreference {
+
+    private static int mDefVal;
+
+    private static final int NODE_LEVEL = R.string.node_saturation_preference;
+
+    public SaturationPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mInterval = context.getResources().getInteger(R.integer.kcal_saturation_preference_interval);
+        mShowSign = false;
+        mUnits = "";
+        mContinuousUpdates = false;
+
+        int[] mAllValues = context.getResources().getIntArray(R.array.kcal_saturation_preference_array);
+        mMinValue = mAllValues[1];
+        mMaxValue = mAllValues[2];
+        mDefaultValueExists = true;
+        mDefVal = mAllValues[0];
+        mDefaultValue = mDefVal;
+        mValue = Integer.parseInt(loadValue(context));
+
+        setPersistent(false);
+    }
+
+    private static String getFile(Context context) {
+        String file = context.getString(NODE_LEVEL);
+        if (FileUtils.fileWritable(file)) {
+            return file;
+        }
+        return null;
+    }
+
+    public static boolean isSupported(Context context) {
+        return FileUtils.fileWritable(getFile(context));
+    }
+
+    public static void restore(Context context) {
+        if (!isSupported(context)) {
+            return;
+        }
+        
+        int[] mAllValues = context.getResources().getIntArray(R.array.kcal_saturation_preference_array);
+        mDefVal = mAllValues[0];
+        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(KcalSettings.KEY_SATURATION, String.valueOf(mDefVal));
+        FileUtils.writeValue(getFile(context), storedValue);
+    }
+
+    public static String loadValue(Context context) {
+        return FileUtils.getFileValue(getFile(context), String.valueOf(mDefVal));
+    }
+
+    private void saveValue(String newValue) {
+        FileUtils.writeValue(getFile(getContext()), newValue);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        editor.putString(KcalSettings.KEY_SATURATION, newValue);
+        editor.apply();
+    }
+
+    @Override
+    protected void changeValue(int newValue) {
+        saveValue(String.valueOf(newValue));
+    }
+}

OPlusExtras/src/org/evolution/oplus/OPlusExtras/Preferences/ValuePreference.java
@@ -1,0 +1,94 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+* Copyright (C) 2021-2022 The Evolution X Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.com/licenses/>.
+*
+*/
+package org.evolution.oplus.OPlusExtras.preferences;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceViewHolder;
+import android.util.AttributeSet;
+
+import org.evolution.oplus.OPlusExtras.kcal.KcalSettings;
+import org.evolution.oplus.OPlusExtras.FileUtils;
+import org.evolution.oplus.OPlusExtras.R;
+
+public class ValuePreference extends CustomSeekBarPreference {
+
+    private static int mDefVal;
+
+    private static final int NODE_LEVEL = R.string.node_value_preference;
+
+    public ValuePreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mInterval = context.getResources().getInteger(R.integer.kcal_value_preference_interval);
+        mShowSign = false;
+        mUnits = "";
+        mContinuousUpdates = false;
+
+        int[] mAllValues = context.getResources().getIntArray(R.array.kcal_value_preference_array);
+        mMinValue = mAllValues[1];
+        mMaxValue = mAllValues[2];
+        mDefaultValueExists = true;
+        mDefVal = mAllValues[0];
+        mDefaultValue = mDefVal;
+        mValue = Integer.parseInt(loadValue(context));
+
+        setPersistent(false);
+    }
+
+    private static String getFile(Context context) {
+        String file = context.getString(NODE_LEVEL);
+        if (FileUtils.fileWritable(file)) {
+            return file;
+        }
+        return null;
+    }
+
+    public static boolean isSupported(Context context) {
+        return FileUtils.fileWritable(getFile(context));
+    }
+
+    public static void restore(Context context) {
+        if (!isSupported(context)) {
+            return;
+        }
+        
+        int[] mAllValues = context.getResources().getIntArray(R.array.kcal_value_preference_array);
+        mDefVal = mAllValues[0];
+        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(KcalSettings.KEY_VALUE, String.valueOf(mDefVal));
+        FileUtils.writeValue(getFile(context), storedValue);
+    }
+
+    public static String loadValue(Context context) {
+        return FileUtils.getFileValue(getFile(context), String.valueOf(mDefVal));
+    }
+
+    private void saveValue(String newValue) {
+        FileUtils.writeValue(getFile(getContext()), newValue);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        editor.putString(KcalSettings.KEY_VALUE, newValue);
+        editor.apply();
+    }
+
+    @Override
+    protected void changeValue(int newValue) {
+        saveValue(String.valueOf(newValue));
+    }
+}

SENSEIIIII/hardware_oplus-1

OPlusExtras/src/org/evolution/oplus/OPlusExtras/Preferences/ContrastPreference.java
@@ -1,0 +1,94 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+* Copyright (C) 2021-2022 The Evolution X Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.com/licenses/>.
+*
+*/
+package org.evolution.oplus.OPlusExtras.preferences;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceViewHolder;
+import android.util.AttributeSet;
+
+import org.evolution.oplus.OPlusExtras.kcal.KcalSettings;
+import org.evolution.oplus.OPlusExtras.FileUtils;
+import org.evolution.oplus.OPlusExtras.R;
+
+public class ContrastPreference extends CustomSeekBarPreference {
+
+    private static int mDefVal;
+
+    private static final int NODE = R.string.node_contrast_preference;
+
+    public ContrastPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mInterval = context.getResources().getInteger(R.integer.kcal_contrast_preference_interval);
+        mShowSign = false;
+        mUnits = "";
+        mContinuousUpdates = false;
+
+        int[] mAllValues = context.getResources().getIntArray(R.array.kcal_contrast_preference_array);
+        mMinValue = mAllValues[1];
+        mMaxValue = mAllValues[2];
+        mDefaultValueExists = true;
+        mDefVal = mAllValues[0];
+        mDefaultValue = mDefVal;
+        mValue = Integer.parseInt(loadValue(context));
+
+        setPersistent(false);
+    }
+
+    private static String getFile(Context context) {
+        String file = context.getString(NODE);
+        if (FileUtils.fileWritable(file)) {
+            return file;
+        }
+        return null;
+    }
+
+    public static boolean isSupported(Context context) {
+        return FileUtils.fileWritable(getFile(context));
+    }
+
+    public static void restore(Context context) {
+        if (!isSupported(context)) {
+            return;
+        }
+
+        int[] mAllValues = context.getResources().getIntArray(R.array.kcal_contrast_preference_array);
+        mDefVal = mAllValues[0];
+        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(KcalSettings.KEY_CONTRAST, String.valueOf(mDefVal));
+        FileUtils.writeValue(getFile(context), storedValue);
+    }
+
+    public static String loadValue(Context context) {
+        return FileUtils.getFileValue(getFile(context), String.valueOf(mDefVal));
+    }
+
+    private void saveValue(String newValue) {
+        FileUtils.writeValue(getFile(getContext()), newValue);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        editor.putString(KcalSettings.KEY_CONTRAST, newValue);
+        editor.apply();
+    }
+
+    @Override
+    protected void changeValue(int newValue) {
+        saveValue(String.valueOf(newValue));
+    }
+}

OPlusExtras/src/org/evolution/oplus/OPlusExtras/Preferences/GreenPreference.java
@@ -1,0 +1,94 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+* Copyright (C) 2021-2022 The Evolution X Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.com/licenses/>.
+*
+*/
+package org.evolution.oplus.OPlusExtras.preferences;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceViewHolder;
+import android.util.AttributeSet;
+
+import org.evolution.oplus.OPlusExtras.kcal.KcalSettings;
+import org.evolution.oplus.OPlusExtras.FileUtils;
+import org.evolution.oplus.OPlusExtras.R;
+
+public class GreenPreference extends CustomSeekBarPreference {
+
+    private static int mDefVal;
+
+    private static final int NODE = R.string.node_green_preference;
+
+    public GreenPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mInterval = context.getResources().getInteger(R.integer.kcal_green_preference_interval);
+        mShowSign = false;
+        mUnits = "";
+        mContinuousUpdates = false;
+
+        int[] mAllValues = context.getResources().getIntArray(R.array.kcal_green_preference_array);
+        mMinValue = mAllValues[1];
+        mMaxValue = mAllValues[2];
+        mDefaultValueExists = true;
+        mDefVal = mAllValues[0];
+        mDefaultValue = mDefVal;
+        mValue = Integer.parseInt(loadValue(context));
+
+        setPersistent(false);
+    }
+
+    private static String getFile(Context context) {
+        String file = context.getString(NODE);
+        if (FileUtils.fileWritable(file)) {
+            return file;
+        }
+        return null;
+    }
+
+    public static boolean isSupported(Context context) {
+        return FileUtils.fileWritable(getFile(context));
+    }
+
+    public static void restore(Context context) {
+        if (!isSupported(context)) {
+            return;
+        }
+
+        int[] mAllValues = context.getResources().getIntArray(R.array.kcal_green_preference_array);
+        mDefVal = mAllValues[0];
+        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(KcalSettings.KEY_GREEN, String.valueOf(mDefVal));
+        FileUtils.writeValue(getFile(context), storedValue);
+    }
+
+    public static String loadValue(Context context) {
+        return FileUtils.getFileValue(getFile(context), String.valueOf(mDefVal));
+    }
+
+    private void saveValue(String newValue) {
+        FileUtils.writeValue(getFile(getContext()), newValue);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        editor.putString(KcalSettings.KEY_GREEN, newValue);
+        editor.apply();
+    }
+
+    @Override
+    protected void changeValue(int newValue) {
+        saveValue(String.valueOf(newValue));
+    }
+}

OPlusExtras/src/org/evolution/oplus/OPlusExtras/Preferences/SaturationPreference.java
@@ -1,0 +1,94 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+* Copyright (C) 2021-2022 The Evolution X Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.com/licenses/>.
+*
+*/
+package org.evolution.oplus.OPlusExtras.preferences;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceViewHolder;
+import android.util.AttributeSet;
+
+import org.evolution.oplus.OPlusExtras.kcal.KcalSettings;
+import org.evolution.oplus.OPlusExtras.FileUtils;
+import org.evolution.oplus.OPlusExtras.R;
+
+public class SaturationPreference extends CustomSeekBarPreference {
+
+    private static int mDefVal;
+
+    private static final int NODE = R.string.node_saturation_preference;
+
+    public SaturationPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mInterval = context.getResources().getInteger(R.integer.kcal_saturation_preference_interval);
+        mShowSign = false;
+        mUnits = "";
+        mContinuousUpdates = false;
+
+        int[] mAllValues = context.getResources().getIntArray(R.array.kcal_saturation_preference_array);
+        mMinValue = mAllValues[1];
+        mMaxValue = mAllValues[2];
+        mDefaultValueExists = true;
+        mDefVal = mAllValues[0];
+        mDefaultValue = mDefVal;
+        mValue = Integer.parseInt(loadValue(context));
+
+        setPersistent(false);
+    }
+
+    private static String getFile(Context context) {
+        String file = context.getString(NODE);
+        if (FileUtils.fileWritable(file)) {
+            return file;
+        }
+        return null;
+    }
+
+    public static boolean isSupported(Context context) {
+        return FileUtils.fileWritable(getFile(context));
+    }
+
+    public static void restore(Context context) {
+        if (!isSupported(context)) {
+            return;
+        }
+
+        int[] mAllValues = context.getResources().getIntArray(R.array.kcal_saturation_preference_array);
+        mDefVal = mAllValues[0];
+        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(KcalSettings.KEY_SATURATION, String.valueOf(mDefVal));
+        FileUtils.writeValue(getFile(context), storedValue);
+    }
+
+    public static String loadValue(Context context) {
+        return FileUtils.getFileValue(getFile(context), String.valueOf(mDefVal));
+    }
+
+    private void saveValue(String newValue) {
+        FileUtils.writeValue(getFile(getContext()), newValue);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        editor.putString(KcalSettings.KEY_SATURATION, newValue);
+        editor.apply();
+    }
+
+    @Override
+    protected void changeValue(int newValue) {
+        saveValue(String.valueOf(newValue));
+    }
+}

OPlusExtras/src/org/evolution/oplus/OPlusExtras/Preferences/ValuePreference.java
@@ -1,0 +1,94 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+* Copyright (C) 2021-2022 The Evolution X Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.com/licenses/>.
+*
+*/
+package org.evolution.oplus.OPlusExtras.preferences;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceViewHolder;
+import android.util.AttributeSet;
+
+import org.evolution.oplus.OPlusExtras.kcal.KcalSettings;
+import org.evolution.oplus.OPlusExtras.FileUtils;
+import org.evolution.oplus.OPlusExtras.R;
+
+public class ValuePreference extends CustomSeekBarPreference {
+
+    private static int mDefVal;
+
+    private static final int NODE = R.string.node_value_preference;
+
+    public ValuePreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mInterval = context.getResources().getInteger(R.integer.kcal_value_preference_interval);
+        mShowSign = false;
+        mUnits = "";
+        mContinuousUpdates = false;
+
+        int[] mAllValues = context.getResources().getIntArray(R.array.kcal_value_preference_array);
+        mMinValue = mAllValues[1];
+        mMaxValue = mAllValues[2];
+        mDefaultValueExists = true;
+        mDefVal = mAllValues[0];
+        mDefaultValue = mDefVal;
+        mValue = Integer.parseInt(loadValue(context));
+
+        setPersistent(false);
+    }
+
+    private static String getFile(Context context) {
+        String file = context.getString(NODE);
+        if (FileUtils.fileWritable(file)) {
+            return file;
+        }
+        return null;
+    }
+
+    public static boolean isSupported(Context context) {
+        return FileUtils.fileWritable(getFile(context));
+    }
+
+    public static void restore(Context context) {
+        if (!isSupported(context)) {
+            return;
+        }
+
+        int[] mAllValues = context.getResources().getIntArray(R.array.kcal_value_preference_array);
+        mDefVal = mAllValues[0];
+        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(KcalSettings.KEY_VALUE, String.valueOf(mDefVal));
+        FileUtils.writeValue(getFile(context), storedValue);
+    }
+
+    public static String loadValue(Context context) {
+        return FileUtils.getFileValue(getFile(context), String.valueOf(mDefVal));
+    }
+
+    private void saveValue(String newValue) {
+        FileUtils.writeValue(getFile(getContext()), newValue);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        editor.putString(KcalSettings.KEY_VALUE, newValue);
+        editor.apply();
+    }
+
+    @Override
+    protected void changeValue(int newValue) {
+        saveValue(String.valueOf(newValue));
+    }
+}